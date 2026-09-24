f509f3e6415a37226e746c405ff1c15364de23e6
missnapokita <138368066+missnapokita@users.noreply.github.com>
2026-09-24 12:47:55 +0800

    Upload plans.js

---
+++ src/plans.js
@@ -0,0 +1,22 @@
+export const VIP_PLANS = Object.freeze({
+  "1_week": {
+    label: "1 Week VIP",
+    amount: 2900,
+    durationMs: 7 * 24 * 60 * 60 * 1000
+  },
+  "1_month": {
+    label: "1 Month VIP",
+    amount: 7900,
+    durationMs: 30 * 24 * 60 * 60 * 1000
+  },
+  "3_months": {
+    label: "3 Months VIP",
+    amount: 19900,
+    durationMs: 90 * 24 * 60 * 60 * 1000
+  },
+  "1_year": {
+    label: "1 Year VIP",
+    amount: 59900,
+    durationMs: 365 * 24 * 60 * 60 * 1000
+  }
+});
